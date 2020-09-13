e597b5991c48e6e60e7f797f68d0f2d72ec60701
sneha310 <49879075+sneha310@users.noreply.github.com>
2020-09-14 00:01:10 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -107,6 +107,14 @@ def login():
     else:
         result = jsonify({"result":"No results found"})
     return result
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
 	
 	
 if __name__ == '__main__':
